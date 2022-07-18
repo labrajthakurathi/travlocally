182038b5aa68a9378f2ff8dba7718a8b4df7e02f
lab raj thakurathi <44740227+labrajthakurathi@users.noreply.github.com>
2022-07-18 16:41:17 -0700

    added app check

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,10 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import { onSnapshot, doc } from "firebase/firestore";
+const {
+	initializeAppCheck,
+	ReCaptchaV3Provider,
+} = require("firebase/app-check");
 
 const firebaseConfig = {
 	apiKey: "AIzaSyBElcnuQpxS2EWEXlJ9pZEQYUZN2eYojMk",
@@ -17,6 +21,15 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const appCheck = initializeAppCheck(app, {
+	provider: new ReCaptchaV3Provider("6Leiev8gAAAAADJSXMVVFcsbhvHbFxyvL4u2O7IZ"),
+
+	// Optional argument. If true, the SDK automatically refreshes App Check
+	// tokens as needed.
+	isTokenAutoRefreshEnabled: true,
+});
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
